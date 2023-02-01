1e566a8eb9423f82e47985d6aaf0d6258a680905
Mingshi <38367310+mingshi1214@users.noreply.github.com>
2023-01-31 22:17:14 -0500

    Add files via upload
    lab1 code tested on gazebo

---
+++ lab1/nodes/l1_motor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import rospy
+import math
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+def publisher_node():
+    """TODO: initialize the publisher node here, \
+            and publish wheel command to the cmd_vel topic')"""
+    cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        twist=Twist()
+        twist.linear.x=0.1
+        twist.angular.z=0.1
+        cmd_pub.publish(twist)
+        rate.sleep()
+
+
+def main():
+    try:
+        rospy.init_node('motor')
+        publisher_node()
+    except rospy.ROSInterruptException:
+        pass
+
+
+if __name__ == "__main__":
+    main()
