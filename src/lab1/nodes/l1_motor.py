db7aebf924484b07fd559e66f139d73e5ab41a3b
yAya-yns <yannis11223@gmail.com>
2023-02-13 08:42:08 -0500

    added readme for lab2, modified gitignore for turtlebot3_simulations, fixed lab1 code

--- src/lab1/nodes/l1_motor.py
+++ src/lab1/nodes/l1_motor.py
@@ -8,11 +8,28 @@ def publisher_node():
     """TODO: initialize the publisher node here, \
             and publish wheel command to the cmd_vel topic')"""
     cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
-    rate = rospy.Rate(10)
+    
+    freq = 10
+    rate = rospy.Rate(freq)
+    dt = 1/freq
+    
+    time = 0
+    x_speed = 0.1
+    z_speed = 0.15
+    t_stage1 = 1/x_speed
+    t_stage2 = t_stage1 + (2 * math.pi) / z_speed
     while not rospy.is_shutdown():
-        twist=Twist()
-        twist.linear.x=0.1
-        twist.angular.z=0.1
+        twist = Twist()
+        if time <= t_stage1:
+            twist.linear.x = x_speed
+            twist.angular.z = 0
+        elif time > t_stage1 and time <= t_stage2:
+            twist.linear.x = 0
+            twist.angular.z = z_speed
+        else:
+            twist.linear.x = 0
+            twist.angular.z = 0
+        time += dt
         cmd_pub.publish(twist)
         rate.sleep()
 
---
+++ src/lab2/README.md
@@ -0,0 +1,11 @@
+Please refer to lab2_tasks_v3.pdf for project objectives.
+
+Starting in docker:
+
+```cd docker && bash start_docker```
+
+Run in seperate terminal (using docker):
+
+```roslaunch rob521_lab2 willowgarage_world.launch```
+
+```roslaunch rob521_lab2 map_view.launch```
