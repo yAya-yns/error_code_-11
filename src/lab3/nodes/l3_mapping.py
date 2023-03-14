9209b60651b028d0d733577d7f40ab3f9ddcd282
mingshi1214 <mingshichi@gmail.com>
2023-03-14 14:29:19 -0400

    leo stuff

--- src/lab3/nodes/l3_mapping.py
+++ src/lab3/nodes/l3_mapping.py
@@ -74,15 +74,17 @@ class OccupancyGripMap:
         self.map_odom_tf.header.stamp = rospy.Time.now()
         self.tf_br.sendTransform(self.map_odom_tf)
 
-    def scan_cb(self, scan_msg):
+    def scan_cb(self, scan_msg : LaserScan):
         # read new laser data and populate map
         # get current odometry robot pose
+        # print(scan_msg)
+        # rospy.loginfo(len(scan_msg.ranges))
         try:
             odom_tf = self.tf_buffer.lookup_transform('odom', 'base_scan', rospy.Time(0)).transform
         except tf2_ros.TransformException:
             rospy.logwarn('Pose from odom lookup failed. Using origin as odom.')
             odom_tf = convert_pose_to_tf(self.map_msg.info.origin)
-
+        # rospy.loginfo(('odom_tf', odom_tf))
         # get odom in frame of map
         odom_map_tf = tf_mat_to_tf(
             np.linalg.inv(tf_to_tf_mat(convert_pose_to_tf(self.map_msg.info.origin))).dot(tf_to_tf_mat(odom_tf))
@@ -92,15 +94,24 @@ class OccupancyGripMap:
         odom_map[1] = odom_map_tf.translation.y
         odom_map[2] = euler_from_ros_quat(odom_map_tf.rotation)[2]
 
+        print(odom_map)
+
+        # rospy.loginfo(('odom_map', odom_map))
+
         # YOUR CODE HERE!!! Loop through each measurement in scan_msg to get the correct angle and
         # x_start and y_start to send to your ray_trace_update function.
+        for i in range(len(scan_msg.ranges)//4):
+        # for i in range(1):
+            self.np_map, self.log_odds = self.ray_trace_update(self.np_map, self.log_odds, int(odom_map[1] * 100), int(odom_map[0] * 100), -odom_map[2] + i * scan_msg.angle_increment + np.pi/2, scan_msg.ranges[i])
+
+        # self.np_map[:, self.np_map.shape[1] // 4] = 1
 
         # publish the message
         self.map_msg.info.map_load_time = rospy.Time.now()
         self.map_msg.data = self.np_map.flatten()
         self.map_pub.publish(self.map_msg)
 
-    def ray_trace_update(self, map, log_odds, x_start, y_start, angle, range_mes):
+    def ray_trace_update(self, map : np.ndarray, log_odds : np.ndarray, x_start : int, y_start : int, angle : float, range_mes : float):
         """
         A ray tracing grid update as described in the lab document.
 
@@ -115,6 +126,36 @@ class OccupancyGripMap:
         # YOUR CODE HERE!!! You should modify the log_odds object and the numpy map based on the outputs from
         # ray_trace and the equations from class. Your numpy map must be an array of int8s with 0 to 100 representing
         # probability of occupancy, and -1 representing unknown.
+        assert map.dtype == np.int16, map.dtype
+        # print(map.shape)
+
+        n_row, n_col = map.shape
+        dir_vec = np.array([np.cos(angle), np.sin(angle)])
+        start_pos = np.array([x_start, y_start])
+        y_dist = n_row - y_start - 1
+        x_dist = n_col - x_start - 1
+        dist = min(range_mes * 100, np.abs(x_dist / np.cos(angle)), np.abs(y_dist / np.sin(angle)))
+        dest = dist * dir_vec + start_pos
+
+        # if np.all(np.logical_and(map.shape - temp_dest1 >= 0, temp_dest1 >= 0)):
+        #     dest = temp_dest1
+        # else:
+        #     dest = temp_dest2
+        print(range_mes)
+        print(y_dist)
+        print(x_dist)
+
+        dest = np.round(dest).astype(int)
+        
+        print(x_start, y_start, dest[0], dest[1])
+        assert np.all(np.logical_and(map.shape - dest >= 0, dest >= 0)), dest
+
+        # print(dest.dtype, y_start, x_start)
+        rr, cc = ray_trace(x_start, y_start, dest[0], dest[1])
+
+        map[rr, cc] = 1
+
+        # print(map)
 
         return map, log_odds
 
